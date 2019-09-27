4d45d3f2a69d139e83916f7779c2f86295e0c6c6
aaayejaaaye <ajones31256@gmail.com>
2019-09-26 20:08:38 -0600

    adding the rest

--- lab3_script.sh
+++ lab3_script.sh
@@ -15,5 +15,15 @@ read name
 #egrep "$regEx" "$name"
 #problem 3
 
-regex='^?([0-9]{3})\-?([0-9]{3})\-?([0-9]{4})' 
-egrep "$regex" "$name"
+#regex='^?([0-9]{3})\-?([0-9]{3})\-?([0-9]{4})' 
+#egrep "$regex" "$name"
+
+#problem 4
+#count the number of emails, list 303 area code phone numbers,store emails from geocities.com into a new file name email_results.
+regexemails='\@'
+regex303='^?([303]{3})\-?([0-9]{3})\-?([0-9]{4})'
+regexgeo='\@geocities.com'
+egrep -c "$regexemails" "$name"
+egrep "$regex303" "$name"
+egrep "$regexgeo" "$name" >> email_results.txt
+
