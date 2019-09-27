d449e902d6be9df5fdba86d166838efe7febafc6
aaayejaaaye <ajones31256@gmail.com>
2019-09-26 19:46:17 -0600

    added #3

--- lab3_script.sh
+++ lab3_script.sh
@@ -6,10 +6,14 @@
 #Documentation
 echo "Enter your filename: "
 read name
-echo "Type a regex"
-read regEX
-echo "File Name : $name"
-echo "Regex :$regEx"
+#echo "Type a regex"
+#read regEX
+#echo "File Name : $name"
+#echo "Regex :$regEx"
 
 #problem 2
-egrep "$regEx" "$name"
+#egrep "$regEx" "$name"
+#problem 3
+
+regex='^?([0-9]{3})\-?([0-9]{3})\-?([0-9]{4})' 
+egrep "$regex" "$name"
